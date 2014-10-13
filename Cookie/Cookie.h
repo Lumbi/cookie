1718d0dbd880ff01cc15b134d80932fb39c741ca
Gabriel Lumbi <gabriel.lumbi@gmail.com>
2014-10-12 21:55:05 -0400

    Initial Commit

---
+++ Cookie/Cookie.h
@@ -0,0 +1,23 @@
+/*
+ *  Cookie.h
+ *  Cookie
+ *
+ *  Created by Gabriel Lumbi on 2014-10-12.
+ *  Copyright (c) 2014 Gabriel Lumbi. All rights reserved.
+ *
+ */
+
+#ifndef Cookie_
+#define Cookie_
+
+/* The classes below are exported */
+#pragma GCC visibility push(default)
+
+class Cookie
+{
+	public:
+		void HelloWorld(const char *);
+};
+
+#pragma GCC visibility pop
+#endif
---
+++ Cookie/CookiePriv.h
@@ -0,0 +1,19 @@
+/*
+ *  CookiePriv.h
+ *  Cookie
+ *
+ *  Created by Gabriel Lumbi on 2014-10-12.
+ *  Copyright (c) 2014 Gabriel Lumbi. All rights reserved.
+ *
+ */
+
+/* The classes below are not exported */
+#pragma GCC visibility push(hidden)
+
+class CookiePriv
+{
+	public:
+		void HelloWorldPriv(const char *);
+};
+
+#pragma GCC visibility pop
